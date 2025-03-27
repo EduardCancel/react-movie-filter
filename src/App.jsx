6846d04ca7e1bf69378777e0ee542f77b13a19e1
IlTuoNome <laTuaEmail@example.com>
2025-03-27 15:47:04 +0100

    Add movie filtering functionality with genre search

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect, use } from "react";
+import movies from "./data/movies"
+
+
+export default function App() {
+
+  const [searchGenre, setSearchGenre] = useState("");   /* Memory the input*/
+  const [filterMovies, setFilterMovies] = useState(movies); /* Memory the movies list */
+
+
+  useEffect(() => {
+    const filteredMovies = movies.filter((movie) => {
+      return movie.genre.toLowerCase().includes(searchGenre.toLowerCase());
+    });
+
+    setFilterMovies(filteredMovies);
+  }, [searchGenre]);
+
+  return (
+
+    <>
+      <h1>Movie Filter</h1>
+
+
+
+    </>
+  )
+
+}
